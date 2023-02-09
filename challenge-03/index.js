93d9bc3cc84cd00e38a715cc4c0555623e0d0870
Rheno001 <renfredreuben@gmail.com>
2023-02-09 14:37:26 +0100

    challenge03 done

---
+++ challenge-03/index.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+const dishes = [{
+    name: "Rice",
+    vegetarian: true
+},
+{
+    name:"Spaghetti",
+    vegetarian: "false"
+}
+{
+    name: "Steak",
+    vegetarian: false
+}{
+    name: "Oats",
+    vegetarian: true
+}
+    
+];
+
+const vegetarian = dishes.filter(
+    => (dishes.vegetarian);
+
+)
+console.log(vegetarian);
